8563474bf7107161ef57a5a05bd5de12df5f41e6
SILVERTHANKS <caduribeiro_@hotmail.com>
2026-01-05 10:35:11 -0300

    Adicionar arianne.py

--- arianne.py
+++
@@ -1,9 +0,0 @@
-try:
-    capacidade = 32767
-    valor = 37727
-    if valor > capacidade :
-      raise OverflowError("erro overflow estouro de 16bits!")
-    else:
-        print("controle de estouro de 16bits ok!")
-except OverflowError as e:
-    print(e)        
